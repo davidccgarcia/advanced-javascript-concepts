f20dbea3c9813e079cc11897d868068d1cd195b3
David García <ccristhiangarcia@gmail.com>
2024-01-07 09:42:59 -0500

    (Practice): Variables de entorno

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { environmentsComponent } from './src/concepts/01-environments';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -16,4 +17,6 @@ document.querySelector('#app').innerHTML = `
     </div>
   </div>
 `
-console.log('Hola Mundo');
+const element = document.querySelector('.card');
+
+environmentsComponent( element );
