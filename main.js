6b3ed778cc0048e96145e8df6bd6f612a7327597
David García <ccristhiangarcia@gmail.com>
2024-01-07 12:02:24 -0500

    Callbacks

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { evironmentsComponent } from './src/concepts/01-environments'
+import { callbacksComponent } from './src/concepts/02-callbacks'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,4 +21,5 @@ document.querySelector('#app').innerHTML = `
 
 const element = document.querySelector('.card');
 
-evironmentsComponent( element );
+// evironmentsComponent( element );
+callbacksComponent( element );
