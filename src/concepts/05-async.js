4ed4784297d19a84c5f07fb4e3dfe92e0f0eadbe
David García <ccristhiangarcia@gmail.com>
2024-01-09 07:55:00 -0500

    (Practice): Async

---
+++ src/concepts/05-async.js
@@ -0,0 +1,26 @@
+import { heroes } from "../data/heroes";
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = ( element ) => {
+
+    const id = '5d86371f97c29d020f1e1f6d';
+
+    findHero(id)
+        .then( name  => element.innerHTML = name)
+        .catch( error => element.innerHTML = error);
+
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
+const findHero = async( id ) => {
+    const hero = heroes.find( hero => hero.id === id);
+
+    return hero?.name;
+}
