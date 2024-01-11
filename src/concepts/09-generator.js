32377af300f1c6a409bd0cca2f980eb78f9ed1cb
David García <ccristhiangarcia@gmail.com>
2024-01-11 09:04:55 -0500

    (Practice): ejemplo de función generadora

--- src/concepts/09-generator.js
+++ src/concepts/09-generator.js
@@ -4,14 +4,29 @@
  */
 export const generatorFuncionsComponent = ( element ) => {
 
-    const myGenerator = myFirstGeneratorFunction();
+    // const myGenerator = myFirstGeneratorFunction();
+    // console.log( myGenerator.next() );
+    const count = counter();
 
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
-    console.log( myGenerator.next() );
+    const button = document.createElement('button');
+    button.innerText = 'Count';
+    element.append( button );
 
+    const renderButton = () => {
+        const { value } = count.next();
+        button.innerText = `Count: ${ value }`;
+    }
+
+    button.addEventListener('click', renderButton );
+
+}
+
+function* counter() {
+    let counter = 0;
+
+    while(true) {
+        yield ++counter;
+    }
 }
 
 function* myFirstGeneratorFunction() {
